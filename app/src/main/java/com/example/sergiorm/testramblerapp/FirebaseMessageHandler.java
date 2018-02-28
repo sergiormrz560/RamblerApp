93b60a12e8609f10bc14945a39f73e84997b984c
brenthompson2 <brenthompson2@gmail.com>
2018-02-28 16:44:54 -0500

    Receiving & Displaying Push Notifications

--- app/src/main/java/com/example/sergiorm/testramblerapp/FirebaseMessageHandler.java
+++ app/src/main/java/com/example/sergiorm/testramblerapp/FirebaseMessageHandler.java
@@ -1,8 +1,12 @@
 package com.example.sergiorm.testramblerapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -11,9 +15,11 @@ import com.google.firebase.messaging.RemoteMessage;
 public class FirebaseMessageHandler extends FirebaseMessagingService {
 
     private static final String TAG = "MessageHandlerFirebase";
+    private static final int REQUEST_CODE = 1;
+    private static final int NOTIFICATION_ID = 6578;
 
     public FirebaseMessageHandler() {
-        
+
     }
 
     // Handle Messages
@@ -41,6 +47,25 @@ public class FirebaseMessageHandler extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
 
             // Send Notification
+            final String title = remoteMessage.getData().get("title");
+            final String message = remoteMessage.getData().get("body");
+            showNotifications(title, message);
         }
     }
+
+    void showNotifications(String title, String msg){
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentText(msg)
+                .setContentTitle(title)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .build();
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID, notification);
+    }
+
 }
