f9b204edcbb138087531076f9c8940976beb8614
SergioRM <sergiormrz560@gmail.com>
2018-02-28 17:46:16 -0500

    Removed title bar and enable zooming.

--- app/src/main/java/com/example/sergiorm/testramblerapp/MainActivity.java
+++ app/src/main/java/com/example/sergiorm/testramblerapp/MainActivity.java
@@ -1,25 +1,27 @@
 package com.example.sergiorm.testramblerapp;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         myWebView = findViewById(R.id.webView);
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setDisplayZoomControls(false);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("https://www.transyrambler.com/");
         myWebView.setWebViewClient(new WebViewClient());
-      //  android.app.ActionBar theActionBar = getActionBar();
-       // theActionBar.hide();
     }
 
     @Override
