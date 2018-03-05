71e861c2f96d7c402e309cf5bde66e2f20501a55
SergioRM <sergiormrz560@gmail.com>
2018-03-04 21:12:46 -0500

    Updated URL to staging site, and enabled safe browsing

--- app/src/main/java/com/example/sergiorm/testramblerapp/FirebaseMessageHandler.java
+++ app/src/main/java/com/example/sergiorm/testramblerapp/FirebaseMessageHandler.java
@@ -3,9 +3,7 @@ package com.example.sergiorm.testramblerapp;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Intent;
-import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
--- app/src/main/java/com/example/sergiorm/testramblerapp/MainActivity.java
+++ app/src/main/java/com/example/sergiorm/testramblerapp/MainActivity.java
@@ -1,27 +1,50 @@
 package com.example.sergiorm.testramblerapp;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+
 public class MainActivity extends Activity {
     private WebView myWebView;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
         myWebView = findViewById(R.id.webView);
-        myWebView.getSettings().setBuiltInZoomControls(true);
-        myWebView.getSettings().setDisplayZoomControls(false);
         WebSettings webSettings = myWebView.getSettings();
+
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
         webSettings.setJavaScriptEnabled(true);
-        myWebView.loadUrl("https://www.transyrambler.com/");
+
+
+        //added code
+        webSettings.setAllowFileAccess(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        myWebView.setWebChromeClient(new WebChromeClient());
+
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setAppCachePath(getApplicationContext().getFilesDir().getAbsolutePath() + "/cache");
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+        //
+
         myWebView.setWebViewClient(new WebViewClient());
+
+
+        myWebView.loadUrl("http://dk5.151.myftpupload.com/");
     }
 
     @Override
